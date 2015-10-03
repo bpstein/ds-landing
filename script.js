357b84b82a7ffcd897137da97d4e507624201440
Ben Stein <bumstain@gmail.com>
2015-10-03 14:20:00 +0100

    Added basic styles and scrollify function

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $.scrollify({
+    section: "section",
+    sectionName: "section-name",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset: 0,
+    scrollbars: true,
+    before: function() {},
+    after: function() {},
+    afterResize: function() {}
+  });
+
+});
