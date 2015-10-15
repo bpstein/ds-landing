2315649c160122ed62c70d7e1bd80f1fc92ee96b
Ben Stein <bumstain@gmail.com>
2015-10-15 23:56:03 +0100

    iPhone fixed element fades in and fades out

--- script.js
+++ script.js
@@ -12,4 +12,49 @@ $(document).ready(function(){
     afterResize: function() {}
   });
 
+  $('iPhone').hide();
+  $('#phoneImage').hide();
+
+  $(window).scroll(function() {
+
+    if ($(this).scrollTop()>0) {
+      $('.iPhone').fadeIn();
+      $('#phoneImage').fadeIn();
+    }
+
+    else {
+      $('.iPhone').fadeOut();
+      $('#phoneImage').hide();
+    }
+  });
+
+// $(window).scroll(function(){
+//   var threshold = 200; // number of pixels before bottom of page that you want to start fading
+//   var op = (($(document).height() - $(window).height()) - $(window).scrollTop()) / threshold;
+//   if( op <= 0 ){
+//     $("#phoneImage").show();
+//   } else {
+//     $("#phoneImage").hide();
+//   }
+//   $("#phoneImage").css("opacity", op ); 
+// });
+
+
+  // $('.phoneImage').show();
+
+  // $(window).scroll(function() {
+  //   if($('#phoneDemo1').is(':visible')) {
+  //     $('.phoneImage').show();
+  //   }
+  // });
+
+//   $(window).scroll(function() {
+//     if ($('.panel home').is(':visible')) {
+//       $('.phoneImage').hide();
+//     } 
+
+//     else ($('.phoneImage').hide())
+//   });
+// });
+
 });
