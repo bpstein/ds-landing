e579a0892ba64aa836d8ddbe4a02fc2e4d59e366
Ben Stein <bumstain@gmail.com>
2015-10-22 01:23:39 +0100

    Compressed images and added animations for word blocks

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+  // Declare global variables
+  var $window = $(window);
+  var $animation_elements = $('.animation-element');
+
+  // Scrollify script for locking panels to screen
   $.scrollify({
     section: "section",
     sectionName: "section-name",
@@ -12,90 +17,73 @@ $(document).ready(function(){
     afterResize: function() {}
   });
 
-  $(window).scroll(function(){
+  // Animate elements if they are in the current viewport
+  function check_if_in_view() {
 
-    // var panelClass = $('.panel').data('section-name');
-    // alert(panelClass);
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
 
-    var scrollPos = $(document).scrollTop();
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
 
-    // Fades in the iPhone placeholder image depending on scroll position
-    if (scrollPos > 1750) {
-      $('#phoneImage').fadeOut(600);
-    }
+      // Check to see if the current element is within the viewport
+      if ((element_bottom_position >= window_top_position) && 
+         (element_top_position <= window_bottom_position)) {
+          $element.addClass('viewable');
+      }
 
-    else {
-      $('#phoneImage').fadeIn(800);
-    }
+      else {
+        $element.removeClass('viewable');
+      }
 
-    if($('.panel').is(':visible')) {
-      $('.response').addClass('response-up');
-      $('.response-up').fadeIn(600);
-    }
+    });
+  }
 
-      //   $(window).scroll(function() {
-//     if ($('.panel home').is(':visible')) {
-//       $('.phoneImage').hide();
-//     } 
+  $window.on('scroll resize', check_if_in_view);
+  $window.trigger('scroll');
 
-//     else ($('.phoneImage').hide())
-//   });
-
-  });
-
-
-  // $('.iPhone').hide();
-
-  // $('.about').scroll(function() {
-  //   ('.iPhone').fadeOut();
-  //   ('#phoneImage').fadeOut();
-  // });
-
-  // $('iPhone').hide();
-  // $('#phoneImage').hide();
-
-  // $(window).scroll(function() {
+});
 
 
-  //   if ($(this).scrollTop()>0) {
-  //     $('.iPhone').fadeOut();
-  //     $('#phoneImage').fadeOut();
-  //     $('detail-text').fadeIn();
+
+
+  // // Declare variables
+  // var $animation_elements = $('.animation-element');
+  // var $window = $(window);
+
+
+  // // Hide elements as the default state
+  // $('.detail-text').hide();
+  // $('.info-block-left').hide();
+  // $('.info-block-middle').hide();
+  // $('.info-block-right').hide();
+
+  // $(window).scroll(function(){
+
+  //   $('.info-block-left').fadeIn(500);
+  //   // $('.info-block-middle')fadeIn(1500);
+  //   // $('.info-block-right')fadeOut(2500);
+
+  //   // var panelClass = $('.panel').data('section-name');
+  //   // alert(panelClass);
+
+  //   var scrollPos = $(document).scrollTop();
+
+  //   // Hide info blocks on the About page 
+  //   $('.info-block-left').hide();
+  //   $('.info-block-middle').hide();
+  //   $('.info-block-right').hide();
+
+  //   // Fades in the iPhone placeholder image depending on scroll position
+  //   if (scrollPos > 1750) {
+  //     $('#phoneImage').fadeOut(600);
   //   }
 
   //   else {
-  //     $('.iPhone').fadeIn();
-  //     $('#phoneImage').show();
+  //     $('#phoneImage').fadeIn(800);
   //   }
   // });
-
-// $(window).scroll(function(){
-//   var threshold = 200; // number of pixels before bottom of page that you want to start fading
-//   var op = (($(document).height() - $(window).height()) - $(window).scrollTop()) / threshold;
-//   if( op <= 0 ){
-//     $("#phoneImage").show();
-//   } else {
-//     $("#phoneImage").hide();
-//   }
-//   $("#phoneImage").css("opacity", op ); 
-// });
-
-
-  // $('.phoneImage').show();
-
-  // $(window).scroll(function() {
-  //   if($('#phoneDemo1').is(':visible')) {
-  //     $('.phoneImage').show();
-  //   }
-  // });
-
-//   $(window).scroll(function() {
-//     if ($('.panel home').is(':visible')) {
-//       $('.phoneImage').hide();
-//     } 
-
-//     else ($('.phoneImage').hide())
-//   });
-// });
-
-});
